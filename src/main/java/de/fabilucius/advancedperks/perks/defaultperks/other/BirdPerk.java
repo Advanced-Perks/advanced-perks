2ff087011823d70254afc00bb5121135becddede
Fabian B <49801250+Fabilucius@users.noreply.github.com>
2022-03-04 21:26:27 +0100

    Adjusted versioning and added "tooltip" to the perks.yml file

--- src/main/java/de/fabilucius/advancedperks/perks/defaultperks/other/BirdPerk.java
+++ src/main/java/de/fabilucius/advancedperks/perks/defaultperks/other/BirdPerk.java
@@ -7,7 +7,6 @@ import de.fabilucius.sympel.item.builder.types.ItemStackBuilder;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;

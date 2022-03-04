4bf92077311510b1740d279dfa3a470c7dcb3e30
Fabian B <49801250+Fabilucius@users.noreply.github.com>
2022-03-04 20:27:12 +0100

    Added the Smelter Perk

---
+++ src/main/java/de/fabilucius/advancedperks/perks/defaultperks/listener/SmelterPerk.java
@@ -0,0 +1,77 @@
+package de.fabilucius.advancedperks.perks.defaultperks.listener;
+
+import de.fabilucius.advancedperks.perks.types.AbstractListenerPerk;
+import de.fabilucius.sympel.item.builder.types.ItemStackBuilder;
+import org.bukkit.Material;
+import org.bukkit.entity.Item;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockDropItemEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Iterator;
+
+public class SmelterPerk extends AbstractListenerPerk {
+    public SmelterPerk() {
+        super("Smelter");
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onBlockBreak(BlockDropItemEvent event) {
+        for (Item item : event.getItems()) {
+            Material material = item.getItemStack().getType();
+            switch (material) {
+                case COAL_ORE:
+                case DEEPSLATE_COAL_ORE:
+                    item.getItemStack().setType(Material.COAL);
+                    break;
+                case IRON_ORE:
+                case DEEPSLATE_IRON_ORE:
+                    item.getItemStack().setType(Material.IRON_INGOT);
+                    break;
+                case GOLD_ORE:
+                case DEEPSLATE_GOLD_ORE:
+                    item.getItemStack().setType(Material.GOLD_INGOT);
+                    break;
+                case COPPER_ORE:
+                case DEEPSLATE_COPPER_ORE:
+                    item.getItemStack().setType(Material.COPPER_INGOT);
+                    break;
+                case DIAMOND_ORE:
+                case DEEPSLATE_DIAMOND_ORE:
+                    item.getItemStack().setType(Material.DIAMOND);
+                    break;
+                case EMERALD_ORE:
+                case DEEPSLATE_EMERALD_ORE:
+                    item.getItemStack().setType(Material.EMERALD);
+                    break;
+                case LAPIS_ORE:
+                case DEEPSLATE_LAPIS_ORE:
+                    item.getItemStack().setType(Material.LAPIS_LAZULI);
+                    break;
+                case NETHER_QUARTZ_ORE:
+                    item.getItemStack().setType(Material.QUARTZ);
+                    break;
+                case REDSTONE_ORE:
+                case DEEPSLATE_REDSTONE_ORE:
+                    item.getItemStack().setType(Material.REDSTONE);
+                    break;
+                case NETHER_GOLD_ORE:
+                    item.getItemStack().setType(Material.GOLD_NUGGET);
+                    break;
+                case ANCIENT_DEBRIS:
+                    item.getItemStack().setType(Material.NETHERITE_SCRAP);
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public ItemStack getDefaultIcon() {
+        return ItemStackBuilder.fromMaterial(Material.MAGMA_CREAM)
+                .setDisplayName(this.getDisplayName())
+                .setDescription(this.getDescription())
+                .build();
+    }
+}

d6b8a2199aefa542c0f81cac31caf6b5eca7403b
Fabian B <49801250+Fabilucius@users.noreply.github.com>
2023-07-13 11:44:49 +0200

    Make the bird perk cross world compatible

--- src/main/java/de/fabilucius/advancedperks/compatability/compats/ChangeWorldCompatability.java
+++ src/main/java/de/fabilucius/advancedperks/compatability/compats/ChangeWorldCompatability.java
@@ -3,6 +3,7 @@ package de.fabilucius.advancedperks.compatability.compats;
 import de.fabilucius.advancedperks.AdvancedPerks;
 import de.fabilucius.advancedperks.commons.BukkitListener;
 import de.fabilucius.advancedperks.compatability.CompatabilityEntity;
+import de.fabilucius.advancedperks.perks.defaultperks.other.BirdPerk;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
@@ -13,6 +14,9 @@ public class ChangeWorldCompatability extends BukkitListener implements Compatab
     public void onPlayerChangedWorld(PlayerChangedWorldEvent event) {
         Player player = event.getPlayer();
         AdvancedPerks.getInstance().getPerkDataRepository().consumePerkData(player, perkData -> {
+            perkData.getActivatedPerks().stream().filter(BirdPerk.class::isInstance).findFirst().ifPresent(perk -> {
+                perk.perkEnable(event.getPlayer());
+            });
             perkData.getActivatedPerks().forEach(perk -> {
                 if (perk.getDisabledWorlds().contains(player.getWorld().getName())) {
                     AdvancedPerks.getInstance().getPerkStateController().disablePerk(player, perk);

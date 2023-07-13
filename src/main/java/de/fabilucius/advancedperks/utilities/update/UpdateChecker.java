4cdc9422f81e3d230bb8ecdbd4bbcc95e609248b
Fabian B <49801250+Fabilucius@users.noreply.github.com>
2023-07-13 15:02:48 +0200

    Decoupled UpdateData class from the Updatechecker class

--- src/main/java/de/fabilucius/advancedperks/utilities/update/UpdateChecker.java
+++ src/main/java/de/fabilucius/advancedperks/utilities/update/UpdateChecker.java
@@ -73,27 +73,6 @@ public class UpdateChecker implements Listener {
                 !AdvancedPerks.getInstance().getDescription().getVersion().equalsIgnoreCase(this.getUpdateData().get().getVersion());
     }
 
-    public static class UpdateData {
-
-        private final String version;
-        private final List<String> changes;
-
-        public UpdateData(String version, List<String> changes) {
-            this.version = version;
-            this.changes = changes;
-        }
-
-        /* The getter and setter of this class */
-
-        public String getVersion() {
-            return version;
-        }
-
-        public List<String> getChanges() {
-            return changes;
-        }
-    }
-
     /* Singleton stuff */
 
     private static UpdateChecker instance;

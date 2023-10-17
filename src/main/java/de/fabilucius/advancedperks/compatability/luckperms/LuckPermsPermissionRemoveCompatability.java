702aae4c2ceb0adb948fa75d1c850542ffbd344f
Fabian Bullik <fabiankbullik@gmail.com>
2023-10-17 14:44:21 +0200

    Included an option to turn the luckperms compatabiltiy on and off

--- src/main/java/de/fabilucius/advancedperks/compatability/luckperms/LuckPermsPermissionRemoveCompatability.java
+++ src/main/java/de/fabilucius/advancedperks/compatability/luckperms/LuckPermsPermissionRemoveCompatability.java
@@ -11,10 +11,17 @@ import org.bukkit.Bukkit;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.logging.Logger;
 
 public class LuckPermsPermissionRemoveCompatability {
 
+    private static final Logger LOGGER = AdvancedPerks.LOGGER;
+
     public LuckPermsPermissionRemoveCompatability() {
+        if (!AdvancedPerks.getInstance().getSettingsConfiguration().isLuckPermsSupportEnabled()) {
+            LOGGER.info("LuckPerms is running but support for its integration was disabled AdvancedPerks will not listen for permission changes.");
+            return;
+        }
         LuckPerms luckPerms = Objects.requireNonNull(Bukkit.getServicesManager().getRegistration(LuckPerms.class)).getProvider();
         luckPerms.getEventBus().subscribe(this, NodeRemoveEvent.class, nodeRemoveEvent -> {
             if (nodeRemoveEvent.isUser() && nodeRemoveEvent.getNode().getType() == NodeType.PERMISSION) {
@@ -22,10 +29,11 @@ public class LuckPermsPermissionRemoveCompatability {
                 PermissionNode permissionNode = (PermissionNode) nodeRemoveEvent.getNode();
                 AdvancedPerks.getInstance().getPerkDataRepository().getPerkDataByUuid(user.getUniqueId()).ifPresent(perkData -> {
                     List<Perk> perksToDisable = perkData.getActivatedPerks().stream().filter(perk -> perk.getPermission().equals(permissionNode.getPermission())).toList();
-                    perksToDisable.forEach(perk -> AdvancedPerks.getInstance().getPerkStateController().disablePerk(perkData.getPlayer(),perk));
+                    perksToDisable.forEach(perk -> AdvancedPerks.getInstance().getPerkStateController().disablePerk(perkData.getPlayer(), perk));
                 });
             }
         });
+        LOGGER.info("LuckPerms is running and you enabled its integration, AdvancedPerks will listen or permission changes and handle them accordingly.");
     }
 
 }

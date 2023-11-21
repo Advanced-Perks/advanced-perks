0be06dc206263d3ac98a584202ed6a056104b13c
Fabian B <49801250+Fabilucius@users.noreply.github.com>
2023-11-21 03:01:22 +0100

    Simplified the process to get a logger instance

--- src/main/java/de/fabilucius/advancedperks/core/logging/APLogger.java
+++ src/main/java/de/fabilucius/advancedperks/core/logging/APLogger.java
@@ -1,15 +1,21 @@
 package de.fabilucius.advancedperks.core.logging;
 
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import de.fabilucius.advancedperks.AdvancedPerks;
+
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
+@Singleton
 public class APLogger extends Logger {
 
-    public APLogger(Logger logger) {
+    @Inject
+    public APLogger(AdvancedPerks plugin) {
         super("advanced-perks logger", null);
         setLevel(Level.ALL);
-        setParent(logger);
+        setParent(plugin.getServer().getLogger());
     }
 
     @Override

c7d903cbcbea92f82399aa80837d3c01aa63e304
Fabian B <49801250+Fabilucius@users.noreply.github.com>
2023-11-23 12:20:09 +0100

    Renamed annotation for better differentiation

--- src/main/java/de/fabilucius/advancedperks/core/command/annotation/CommandIdentifier.java
+++ src/main/java/de/fabilucius/advancedperks/core/command/annotation/CommandIdentifier.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Identifier {
+public @interface CommandIdentifier {
 
     String value();
 

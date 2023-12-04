13e8a4fd0a9ba483738f2d92fa0b10dcaab24670
Fabian B <49801250+Fabilucius@users.noreply.github.com>
2023-12-04 02:04:26 +0100

    Implemented the insomnia perk

---
+++ src/main/java/de/fabilucius/advancedperks/perk/types/TaskPerk.java
@@ -0,0 +1,9 @@
+package de.fabilucius.advancedperks.perk.types;
+
+import de.fabilucius.advancedperks.AdvancedPerks;
+
+public interface TaskPerk {
+
+    void registerTasks(AdvancedPerks advancedPerks);
+
+}

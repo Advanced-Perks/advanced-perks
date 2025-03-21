c8b22328db0a2822da2ed8aa52501670b1eab1b0
Fabian B <49801250+alystor@users.noreply.github.com>
2025-03-21 02:33:41 +0100

    Update ActivationIndicator.kt

--- src/main/kotlin/de/fabilucius/advancedperks/guisystem/model/ActivationIndicator.kt
+++ src/main/kotlin/de/fabilucius/advancedperks/guisystem/model/ActivationIndicator.kt
@@ -2,7 +2,7 @@ package de.fabilucius.advancedperks.guisystem.model
 
 enum class ActivationIndicator {
 
-    //TODO implement this
+    //TODO implement in a future update
     ENCHANTED_PERK_BUTTON,
     PERK_TOGGLE_BUTTON,
     ALL_INDICATORS;
